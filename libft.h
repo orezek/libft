abc279752a5d18ff89c1d0541921dfb8b4c701d6
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-18 11:19:54 +0200

    Update libft.h file add all function prototypes except stnstr prototype

--- libft.h
+++ libft.h
@@ -6,12 +6,68 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:18:02 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/17 11:20:00 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/18 11:18:40 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
 
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
+// part 1
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *block, int c, size_t size);
+void	ft_bzero(void *block, size_t size);
+// redo the restrit qualifier
+void	*ft_memcpy(void *restrict to, const void *restrict from, size_t size);
+void	*ft_memmove(void *to, const void *from, size_t size);
+// redo the restrit qualifier
+size_t	ft_strlcpy(char *restrict to, const char *restrict from, size_t size);
+// redo the restrit qualifier
+size_t	ft_strlcat(char *restrict to, const char *restrict from, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *string, int c);
+char	*ft_strrchr(const char *string, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t size);
+void	*ft_memchr(const void *block, int c, size_t size);
+int		ft_memcmp(const void *a1, const void *a2, size_t size);
+// strnstr - missing prototype in libc
+int		ft_atoi(const char *string);
+// Use malloc()
+void	*ft_calloc(size_t count, size_t eltsize);
+char	*ft_strdup(const char *s);
+// part 2
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+// bonus
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+#endif
