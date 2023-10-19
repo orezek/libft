46a1c60a046fe14db2e8e5b05bc71fa233f19533
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-19 15:29:48 +0200

    Update: implement ft_isdigit function

--- part1/ft_isdigit.c
+++ part1/ft_isdigit.c
@@ -6,8 +6,20 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:29:49 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:29:56 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/19 15:27:48 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+int	ft_isdigit(int c)
+{
+	unsigned char	digit;
+
+	if (c == EOF)
+		return (0);
+	digit = (unsigned char) c;
+	if (digit >= '0' || digit <= '9' )
+		return (1);
+	return (0);
+}
