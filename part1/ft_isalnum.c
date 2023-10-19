8f1740aa2b5c1c538b24e27280ff449f9d5c5097
O.R <orezek@seznam.cz>
2023-10-19 18:09:29 +0200

    Update: ft_isalnum complete

--- part1/ft_isalnum.c
+++ part1/ft_isalnum.c
@@ -3,11 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:29:11 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:29:16 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/19 17:50:07 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+int	ft_isalnum(int c)
+{
+	unsigned char	letter;
+
+	if (c == EOF)
+		return (0);
+	letter = (unsigned char) c;
+	if (ft_isalpha(letter) || ft_isdigit(letter))
+		return (1);
+	return (0);
+}
