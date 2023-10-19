3745c47e6f4a252763bd0dbbf84c90a05a7d1474
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-19 15:42:13 +0200

    Fix: ft_isdigit.c logical error in the conditional statement, chaning || to &&

--- part1/ft_isdigit.c
+++ part1/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:29:49 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/19 15:27:48 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/19 15:36:41 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_isdigit(int c)
 	if (c == EOF)
 		return (0);
 	digit = (unsigned char) c;
-	if (digit >= '0' || digit <= '9' )
+	if (digit >= '0' && digit <= '9' )
 		return (1);
 	return (0);
 }
