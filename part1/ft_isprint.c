4ad65cd617ac441adee1d483a0c2f4bb82fd8f46
O.R <orezek@seznam.cz>
2023-10-19 18:37:14 +0200

    Add: ft_isprint function

--- part1/ft_isprint.c
+++ part1/ft_isprint.c
@@ -3,11 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:06 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:30:08 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/19 18:35:47 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+static int ft_isgraph(int c)
+{
+	unsigned char	letter;
+
+	if (c == EOF)
+		return (0);
+	letter = (unsigned char) c;
+	if (letter >= 33 && letter <= 126)
+		return (1);
+	return (0);
+}
+
+int ft_isprint(int c)
+{
+	unsigned char	letter;
+
+	if (c == EOF)
+		return (0);
+	letter = (unsigned char) c;
+	if (ft_isgraph(letter) && letter == 32)
+		return (1);
+	return (0);
+}
