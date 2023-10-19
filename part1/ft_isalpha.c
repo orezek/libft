e7dccf54171857eb8b3d594d82a6398f8616b56a
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-19 12:47:12 +0200

    Update: ft_isalpha.c implement

--- part1/ft_isalpha.c
+++ part1/ft_isalpha.c
@@ -6,13 +6,30 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:29:30 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:41:32 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/19 12:17:11 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+static	int	ft_islower(int c)
+{
+	if (c >= 97 & c <= 122)
+		return (1);
+	return (0);
+}
+
+static	int	ft_isupper(int c)
+{
+	if (c >= 65 & c <= 90)
+		return (1);
+	return (0);
+}
+
 int	ft_isalpha(int c)
 {
-	;
+	if (ft_islower(c) || ft_isupper(c))
+		return (1);
+	else
+		return (0);
 }
