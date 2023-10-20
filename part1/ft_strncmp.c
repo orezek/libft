c59b8e52c07ed0627c6fdfde4b513e9b79de67d7
O.R <orezek@seznam.cz>
2023-10-20 18:29:07 +0200

    Fix: ft_strncmp.c fix syntax to conform to the norm

--- part1/ft_strncmp.c
+++ part1/ft_strncmp.c
@@ -6,24 +6,24 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:43 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/20 18:05:33 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/20 18:27:25 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t size)
+int	ft_strncmp(const char *s1, const char *s2, size_t size)
 {
-	int start;
+	int	start;
 
 	start = 0;
-	while(start < size)
+	while (start < size)
 	{
 		if (*(s1 + start) == '\0' && *(s2 + start) == '\0')
-			break;
-		if(*(s1 + start) < *(s2 + start))
+			break ;
+		else if (*(s1 + start) < *(s2 + start))
 			return (*(s1 + start) - *(s2 + start));
-		else if(*(s1 + start) > *(s2 + start))
+		else if (*(s1 + start) > *(s2 + start))
 			return (*(s1 + start) - *(s2 + start));
 		start++;
 	}
