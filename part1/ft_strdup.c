71b29352f722eae1ec7e0df38cb2b0dc7722a5f7
O.R <orezek@seznam.cz>
2023-10-20 20:13:54 +0200

    Update: complete ft_strdup.c

--- part1/ft_strdup.c
+++ part1/ft_strdup.c
@@ -1,13 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_stdup.c                                         :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:41 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:30:43 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/20 20:11:20 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*newstr;
+	size_t	strlen;
+
+	strlen = ft_strlen(s);
+	newstr = malloc((strlen + 1) * sizeof(char));
+	if (newstr == NULL)
+		return (NULL);
+	while (*s)
+	{
+		*newstr = *s;
+		s++;
+		newstr++;
+	}
+	*newstr = '\0';
+	return (newstr - strlen);
+}
