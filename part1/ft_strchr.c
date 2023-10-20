b3d504d9a72113ce8088609ef88dc25c85073885
O.R <orezek@seznam.cz>
2023-10-20 10:12:34 +0200

    Update: ft_strchr.c complete

--- part1/ft_strchr.c
+++ part1/ft_strchr.c
@@ -3,11 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:31:12 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:31:13 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/20 10:08:41 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+char	*strchr(const char *string, int c)
+{
+	char	*ptr;
+
+	ptr = string;
+	if (c == '\0')
+		return (string + ft_strelen(string) + 1);
+	while (*string)
+	{
+		if (*string == c)
+			return (string);
+		string++;
+	}
+	return (NULL);
+}
