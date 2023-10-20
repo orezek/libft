f9d196c315b800bb0f3605d38d2566cd327dabfa
O.R <orezek@seznam.cz>
2023-10-20 12:23:47 +0200

    Update: function ft_strrchr complete

--- part1/ft_strrchr.c
+++ part1/ft_strrchr.c
@@ -3,11 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:58 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:37:00 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/20 12:12:47 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+char	*ft_strrchr(const char *string, int c)
+{
+	int	len;
+
+	len = ft_strlen(string);
+	if (c == '\0')
+		return ((char *) string + ft_strlen(string) + 1);
+	while (len >= 0)
+	{
+		if (*(string + len) == c)
+			return ((char *) string + len);
+		len--;
+	}
+	return (NULL);
+}
