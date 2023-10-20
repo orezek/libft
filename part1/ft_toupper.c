88c367e3141c7243940dad4f4bcf48db30cbeb0e
O.R <orezek@seznam.cz>
2023-10-20 09:15:35 +0200

    Update: ft_toupper.c complete

--- part1/ft_toupper.c
+++ part1/ft_toupper.c
@@ -3,11 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:37:08 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:37:11 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/20 09:14:11 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+static	int	ft_islower(int c)
+{
+	unsigned char	letter;
+
+	if (c == EOF)
+		return (0);
+	letter = (unsigned char) c;
+	if (letter >= 'a' && letter <= 'z')
+		return (1);
+	return (0);
+}
+
+static	int	ft_isupper(int c)
+{
+	unsigned char	letter;
+
+	if (c == EOF)
+		return (0);
+	letter = (unsigned char) c;
+	if (letter >= 'A' & letter <= 'Z')
+		return (1);
+	return (0);
+}
+
+int	ft_toupper(int c)
+{
+	if (ft_isalpha(c) && ft_islower(c))
+		return (c - 32);
+	return (c);
+}
