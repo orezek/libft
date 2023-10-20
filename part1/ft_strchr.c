c93e45693371115d7cc9668051c017bf56ede595
O.R <orezek@seznam.cz>
2023-10-20 12:01:11 +0200

    Fix: ft_strchr casting the return type to char * pointer from const char pointer

--- part1/ft_strchr.c
+++ part1/ft_strchr.c
@@ -6,23 +6,20 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:31:12 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/20 10:08:41 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/20 11:58:53 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-char	*strchr(const char *string, int c)
+char	*ft_strchr(const char *string, int c)
 {
-	char	*ptr;
-
-	ptr = string;
 	if (c == '\0')
-		return (string + ft_strelen(string) + 1);
+		return ((char *) string + ft_strlen(string) + 1);
 	while (*string)
 	{
 		if (*string == c)
-			return (string);
+			return ((char *) string);
 		string++;
 	}
 	return (NULL);
