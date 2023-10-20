3f12a34ec598e1f4b61ca1c8d45297dfd237d529
O.R <orezek@seznam.cz>
2023-10-20 23:56:23 +0200

    Update: ft_memset.c complete

--- part1/ft_memset.c
+++ part1/ft_memset.c
@@ -3,11 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:36 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:30:38 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/20 23:45:42 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	*ft_memset(void *block, int c, size_t size)
+{
+	unsigned char	filler;
+	unsigned char	*ptr;
+	int				limit;
+
+	filler = (unsigned char) c;
+	ptr = (unsigned char *) block;
+	limit = 0;
+	while (limit < size)
+	{
+		*ptr = filler;
+		limit++;
+		ptr++;
+	}
+	return (ptr - size);
+}
