80dffe5702b661cd04dfe4afde9a8d1daaf1c0bb
O.R <orezek@seznam.cz>
2023-10-21 19:36:03 +0200

    Update: complete function ft_bzero

--- part1/ft_bzero.c
+++ part1/ft_bzero.c
@@ -3,11 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:28:46 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:28:53 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/21 19:32:10 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	ft_bzero(void *block, size_t size)
+{
+	int				start;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *) block;
+	start = 0;
+	while (start < size)
+	{
+		*(ptr + start) = 0;
+		start++;
+	}
+}
