1a23739bc6e303c4f68439daf126ee6919fe2dbe
O.R <orezek@seznam.cz>
2023-10-21 09:05:00 +0200

    Update complete ft_calloc function

--- part1/ft_calloc.c
+++ part1/ft_calloc.c
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:29:00 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:29:04 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/21 09:03:59 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	*ft_calloc(size_t count, size_t eltsize)
+{
+	void	*ptr;
+
+	ptr = malloc(count * eltsize);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memset(ptr, 0, sizeof(int));
+	return (ptr);
+}
