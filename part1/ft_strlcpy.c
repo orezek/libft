858668945c0930785f44f2cb70dbe59a9153e7e4
O.R <orezek@seznam.cz>
2023-10-22 17:43:22 +0200

    Update: complete funciton ft_strlcpy

--- part1/ft_strlcpy.c
+++ part1/ft_strlcpy.c
@@ -1,13 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_stlcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:58 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:31:03 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/22 17:41:02 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+size_t	ft_strlcpy(char *to, const char *from, size_t size)
+{
+	int			src_size;
+	int			sizet;
+	const char	*end;
+
+	sizet = (int) size;
+	src_size = ft_strlen(from);
+	end = from + (sizet - 1);
+	if (sizet > src_size)
+	{
+		while (*from)
+			*(to++) = *(from++);
+		*to = '\0';
+	}
+	else if (sizet > 0 && sizet <= src_size)
+	{
+		while (from < end)
+			*(to++) = *(from++);
+		*to = '\0';
+	}
+	return ((size_t) src_size);
+}
