2319e0f40de3779e16b33252de17bc95b234c12e
O.R <orezek@seznam.cz>
2023-10-23 08:01:33 +0200

    Update 1/2 complete ft_strlcat function

--- part1/ft_strlcat.c
+++ part1/ft_strlcat.c
@@ -1,13 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_stlcat.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:47 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:30:49 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/23 07:59:42 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+size_t	ft_strlcat(char *to, const char *from, size_t size)
+{
+	int		src_len;
+	int		dst_len;
+	size_t	step;
+
+	src_len = ft_strlen(from);
+	dst_len = ft_strlen(to);
+	step = dst_len;
+	to += dst_len;
+	if (src_len + dst_len >= (int) size)
+	{
+		while(step++ < size - 1)
+			*(to++) = *(from++);
+		*to = '\0';
+	}
+	else
+		step = dst_len;
+		while (step++ < size)
+			*(to++) = *(from++);
+		*to = '\0';
+	return (src_len + dst_len);
+}
