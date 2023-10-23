546f5bbec05ff819d0869fb131d8211ec0fb9550
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-23 13:38:05 +0200

    Fix: redu the old implementation of ft_strnstr

--- part1/ft_strnstr.c
+++ part1/ft_strnstr.c
@@ -6,90 +6,48 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:50 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/23 11:28:33 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/23 13:30:52 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-// needle = string to find
-// haystack = string to search in for
-// Empty needle => return haystack
-// Haystack < needle -> Null
-// Needle not found -> Null
-// Only the first occurance
-
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+static char	*ft_search_for_sub(const char *str, const char *to_find, size_t len)
 {
-	unsigned int	i;
+	size_t	step;
+	int		inner_step;
+	int		success_flag;
 
-	i = 1;
-	if (n < 1)
-		return (0);
-	while (*s1 && (*s1 == *s2) && (i < n))
-	{
-		s1++;
-		s2++;
-		i++;
-	}
-	if (*s1 < *s2)
-		return (-1);
-	else if (*s1 > *s2)
-		return (1);
-	return (0);
-}
-
-int	ft_len(char *str)
-{
-	int	counter;
-
-	counter = 0;
-	while (*str)
-	{
-		str++;
-		counter++;
-	}
-	return (counter);
-}
-
-char	*ft_search_for_substring(char *str, char *to_find)
-{
-	int	step;
-	int	inner_step;
-	int	success_flag;
-	int	length_str;
-
-	length_str = ft_len(str);
 	step = 0;
-	while (step <= length_str)
+	while (step < len)
 	{
 		inner_step = step;
 		success_flag = 0;
-		while (inner_step < (ft_len(to_find) + step))
+		while (inner_step < (int)(ft_strlen(to_find) + step))
 		{
-			if (str[inner_step] == to_find[inner_step - step])
+			if (*(str + inner_step) == *(to_find + inner_step - step))
 				success_flag += 1;
 			inner_step++;
 		}
-		if (success_flag == ft_len(to_find))
-			return (&str[step]);
+		if (success_flag == (int) ft_strlen(to_find))
+			return ((char *) str + step);
 		step++;
 	}
 	return (NULL);
 }
 
-char	*ft_strstr(char *str, char *to_find)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	if (*to_find == '\0')
-		return (str);
-	if (ft_len(str) < ft_len(to_find))
+	if (*little == '\0')
+		return ((char *) big);
+	if (ft_strlen(big) < ft_strlen(little))
 		return (NULL);
-	if (ft_len(str) == ft_len(to_find))
+	if (ft_strlen(big) == ft_strlen(little))
 	{
-		if (ft_strncmp(str, to_find, ft_len(str)) == 0)
-			return (str);
+		if (ft_strncmp(big, little, ft_strlen(big)) == 0)
+			return ((char *) big);
 		else
 			return (NULL);
 	}
-	return (ft_search_for_substring(str, to_find));
+	return (ft_search_for_sub(big, little, len));
 }
