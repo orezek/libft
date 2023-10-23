1a0441279da67e79009b82506b12620ec7256255
O.R <orezek@seznam.cz>
2023-10-23 08:01:56 +0200

    Test: update test files

--- tests/tst_ft_strlcat.c
+++ tests/tst_ft_strlcat.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tst_ft_strlcpy.c                                   :+:      :+:    :+:   */
+/*   tst_ft_strlcat.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/22 17:09:17 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/10/22 17:51:10 by aldokezer        ###   ########.fr       */
+/*   Created: 2023/10/23 06:57:36 by aldokezer         #+#    #+#             */
+/*   Updated: 2023/10/23 07:57:51 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-//cc -Wall -Wextra -Werror tst_ft_strlcpy.c ../part1/ft_strlcpy.c ../part1/ft_strlen.c && ./a.out
+
 int	main (void)
 {
-	char	src[] = "ahoj";
-	char	des[] = "x";
-	int		result;
+	const char	src[] = "ahoj";
+	char		des[] = "xxxx";
+	char		*ptr;
+	int			result;
 
-	result = ft_strlcpy(des, src, 5);
-	printf("%d\n", result);
-	printf("%s\n", des);
-	return (0);
+
+	result = ft_strlcat(des, src, 16);
+	ptr = des;
+	//printf("%d\n", result);
+	//write(1, "\n", 1);
+	write(1, ptr, sizeof(des));
+	write(1, "\n", 1);
+
 }
