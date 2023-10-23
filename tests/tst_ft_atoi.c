6b927b0adf1d4904c34552589b0ccd72671d9a7e
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-23 17:14:49 +0200

    Test: create test case for ft_atoi file

---
+++ tests/tst_ft_atoi.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tst_ft_atoi.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 16:15:41 by orezek            #+#    #+#             */
+/*   Updated: 2023/10/23 16:53:59 by orezek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+//cc -Wall -Wextra -Werror tst_ft_atoi.c ../part1/ft_atoi.c ../part1/ft_isdigit.c && ./a.out
+int	main (void)
+
+{
+	char	str[] = "+123.45";
+	int		result;
+	int		result1;
+
+	result = ft_atoi(str);
+	result1 = atoi(str);
+	printf("%d\n", result);
+	printf("%d\n", result1);
+}
