e5265bcbbd86b1efd22aca95d2bd02d48605a27e
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-23 17:06:18 +0200

    Update: complete ft_atoi function

--- part1/ft_atoi.c
+++ part1/ft_atoi.c
@@ -6,8 +6,48 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:28:16 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:28:35 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/23 17:03:34 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+static int	ft_isspace(int c)
+{
+	unsigned char	l;
+
+	l = (unsigned char) c;
+	if (l == ' ' || l == '\f' || l == '\n'
+		|| l == '\r' || l == '\t' || l == '\v')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *string)
+{
+	int	is_negative;
+	int	result;
+
+	result = 0;
+	is_negative = 0;
+	while (ft_isspace(*string))
+		string++;
+	if (*string == '-')
+	{
+		is_negative = 1;
+		string++;
+	}
+	else if (*string == '+')
+		string++;
+	while (isdigit(*string))
+	{
+		if (!isdigit(*(string + 1)))
+		{
+			if (is_negative)
+				return ((result += ((int) *string) - 48) * -1);
+			return (result += ((int) *string) - 48);
+		}
+		result = (result + (((int) *string++) - 48)) * 10;
+	}
+	return (0);
+}
