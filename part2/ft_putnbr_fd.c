9f93dce9a3d4f288dfe6dd0ad6429ef8551ee0c7
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-24 12:04:03 +0200

    Update: complete putnbr_fd function

--- part2/ft_putnbr_fd.c
+++ part2/ft_putnbr_fd.c
@@ -6,8 +6,37 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:37:40 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:37:41 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/24 11:49:45 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char			c;
+	long long		number;
+	long long		reversed;
+
+	number = (long long) n;
+	reversed = 0;
+	if (number < 0)
+	{
+		number *= -1;
+		write(fd, "-", 1);
+	}
+	else if (number == 0)
+		write(fd, "0", 1);
+	while (number != 0)
+	{
+		reversed = reversed * 10 + (number % 10);
+		number /= 10;
+	}
+	while (reversed != 0)
+	{
+		c = ((char)(reversed % 10)) + 48;
+		write(fd, &c, 1);
+		reversed /= 10;
+	}
+
+}
