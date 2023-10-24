74a4c3d97c7a5f3b7f012c3a0a3d1b4c03db8621
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-24 14:44:16 +0200

    InProgress: ft_itoa function not complete

--- part2/ft_itoa.c
+++ part2/ft_itoa.c
@@ -6,8 +6,77 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:37:20 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:37:21 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/24 14:37:11 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+static int	ft_nlength(long long n)
+{
+	int	counter;
+
+	counter = 0;
+	while (n != 0)
+	{
+		counter++;
+		n /= 10;
+	}
+	printf("%d\n", counter);
+	return (counter);
+}
+
+static long long	ft_nreverse(long long n)
+{
+	long long	number;
+
+	number = 0;
+	while (n != 0)
+	{
+		number = number * 10 + (n % 10);
+		n /= 10;
+	}
+	printf("%lld\n", number);
+	return (number);
+}
+
+static char	*ft_nconvert(long long n, char *ptr, int flag)
+{
+	char	*nptr;
+
+	nptr = ptr;
+	if (flag)
+		*ptr++ = '-';
+	while (n != 0)
+	{
+		*ptr++ = (n % 10) + 48;
+		n /= 10;
+	}
+	*ptr = '\0';
+	return (nptr);
+}
+
+char	*ft_itoa(int n)
+{
+	long long	number;
+	int			n_length;
+	int			is_negative;
+	char		*ptr;
+
+	is_negative = 0;
+	number = (long long)n;
+	if (number < 0)
+	{
+		number *= -1;
+		is_negative = 1;
+	}
+	n_length = ft_nlength(number);
+	if (is_negative)
+		ptr = malloc((n_length + 2) * sizeof(char));
+	ptr = malloc((n_length + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	number = ft_nreverse(number);
+	printf("%lld\n", number);
+	return (ft_nconvert(number, ptr, is_negative));
+}
