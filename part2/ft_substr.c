4412af7418b784f6913dcba69b50628bbe55a4ff
O.R <orezek@seznam.cz>
2023-10-25 17:56:59 +0200

    Update: complete ft_substr.c function

--- part2/ft_substr.c
+++ part2/ft_substr.c
@@ -3,11 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:16 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:38:17 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/25 17:55:31 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char		*ptr;
+	int			size;
+
+	size = (int) len;
+	if (start > ft_strlen(s) || len > ft_strlen(s))
+		return (NULL);
+	ptr = malloc(((int)len + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	s += (start);
+	while(len-- > 0)
+			*ptr++ = *s++;
+	*ptr = '\0';
+	return (ptr - size);
+}
