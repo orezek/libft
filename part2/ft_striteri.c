eeaaec2e9955424f4695506e76d2c29463a8a7aa
O.R <orezek@seznam.cz>
2023-10-25 16:33:32 +0200

    Update: complete ft_striteri function

--- part2/ft_striteri.c
+++ part2/ft_striteri.c
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:37:56 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:37:58 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/25 16:17:38 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int		str_len;
+	int		index;
+
+	str_len = ft_strlen(s);
+	index = 0;
+	while (*s)
+		f(index++, s++);
+}
