336f48199d85222cee9d7d0e88c6f2ae3050025a
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-25 10:11:44 +0200

    Update: implement reverse function to reverse the int value

--- part2/ft_putnbr_fd.c
+++ part2/ft_putnbr_fd.c
@@ -6,11 +6,43 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:37:40 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/24 11:49:45 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/25 10:01:11 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+static int	ft_nzero(long n)
+{
+	int			result;
+	long long	number;
+
+	number = n;
+	result = 0;
+	while (n != 0 && n % 10 == 0)
+	{
+		result++;
+		n /= 10;
+	}
+	return (result);
+}
+
+static long long	ft_nreverse(long n)
+{
+	long long	reversed;
+	int			n_zero;
+
+	reversed = 0;
+	n_zero = ft_nzero(n);
+	while (n != 0)
+	{
+		reversed = reversed * 10 + (n % 10);
+		n /= 10;
+	}
+	while (n_zero-- > 0)
+		reversed *= 10;
+	return (reversed);
+}
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -27,11 +59,7 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	else if (number == 0)
 		write(fd, "0", 1);
-	while (number != 0)
-	{
-		reversed = reversed * 10 + (number % 10);
-		number /= 10;
-	}
+	reversed = ft_nreverse(number);
 	while (reversed != 0)
 	{
 		c = ((char)(reversed % 10)) + 48;
