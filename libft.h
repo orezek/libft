b6d8971b95b010f094f7a7c2efa4f4aaacf0c336
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-26 15:50:34 +0200

    Fix: rename parameter in ft_lstadd and ft_lstadd_back from ne to new

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:18:02 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/23 13:03:31 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/26 15:49:42 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,10 +67,10 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 // bonus
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *ne);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *ne);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
