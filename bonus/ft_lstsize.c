cc91a26cb18715e52e870f13cbee1051db674b25
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-26 16:12:25 +0200

    Fix: remove from return statement NULL pointer and add negative value instead

--- bonus/ft_lstsize.c
+++ bonus/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:39:37 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/26 15:46:04 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/26 16:04:51 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_lstsize(t_list *lst)
 	int	counter;
 
 	if (lst == NULL)
-		return (NULL);
+		return (-1);
 	counter = 1;
 	while (lst->next != NULL)
 	{
