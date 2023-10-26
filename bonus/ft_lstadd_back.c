d075e022af80866029329fcef878f251602df115
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-26 16:13:16 +0200

    Update: complete ft_lstadd_back function

--- bonus/ft_lstadd_back.c
+++ bonus/ft_lstadd_back.c
@@ -6,8 +6,17 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:39 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:38:40 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/26 15:58:59 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	last = NULL;
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
