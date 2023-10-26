74775389a5effc03b546ff2f86a2fe686e8333b7
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-26 15:48:33 +0200

    Fix: add condition to check for NULL pointer on the entry of the function.

--- bonus/ft_lstsize.c
+++ bonus/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:39:37 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/26 12:59:29 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/26 15:46:04 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ int	ft_lstsize(t_list *lst)
 {
 	int	counter;
 
-	counter = 0;
-	while (lst++ ->next != NULL)
+	if (lst == NULL)
+		return (NULL);
+	counter = 1;
+	while (lst->next != NULL)
+	{
 		counter++;
+		lst = lst->next;
+	}
 	return (counter);
 }
