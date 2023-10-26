05c3850e08a5e459dd5358bc2fc70c09bab65a6b
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-26 10:04:13 +0200

    Update: complete ft_lstnew function

--- bonus/ft_lstnew.c
+++ bonus/ft_lstnew.c
@@ -6,8 +6,18 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:39:20 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:39:33 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/26 09:43:25 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	(*node).content = content;
+	(*node).next = NULL;
+	return (node);
+}
