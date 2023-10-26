8713bbba25f3277820f7f155351817feaced77ce
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-26 15:49:17 +0200

    Update: complete ft_lstlast function

--- bonus/ft_lstlast.c
+++ bonus/ft_lstlast.c
@@ -6,8 +6,17 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:39:04 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:39:05 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/26 15:46:59 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
