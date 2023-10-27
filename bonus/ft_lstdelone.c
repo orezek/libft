e491bec3b78507cccb59e2acce8ce4a1c16d47da
Oldrich Rezek <orezek@c1r10s5.42prague.com>
2023-10-27 14:39:18 +0200

    Update: complete ft_lstdelone function

--- bonus/ft_lstdelone.c
+++ bonus/ft_lstdelone.c
@@ -6,8 +6,14 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:52 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:38:54 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/27 13:21:01 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst != NULL && del != NULL)
+		(*del)(lst->content);
+}
