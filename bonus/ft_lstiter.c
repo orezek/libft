01b4395e1b324873cce41fc3778320717c20880f
O.R <orezek@seznam.cz>
2023-10-27 22:49:41 +0200

    Update: complete ft_lstiter function

--- bonus/ft_lstiter.c
+++ bonus/ft_lstiter.c
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:56 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:38:57 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/27 22:48:41 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst != NULL && f != NULL)
+		while ((*lst).next != NULL)
+		{
+			f(lst->content);
+			lst = lst->next;
+		}
+		f(lst->content);
+}
