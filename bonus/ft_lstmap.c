b9f608e10f18187bb511043b0b1326ae0e2e8bd8
O.R <orezek@seznam.cz>
2023-10-28 21:28:10 +0200

    Update: ft_lstmap function renaming first for head for clarity

--- bonus/ft_lstmap.c
+++ bonus/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:39:15 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/28 19:12:22 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/28 21:05:43 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*first;
+	t_list	*head;
 
-	first = NULL;
+	head = NULL;
 	while (lst != NULL)
 	{
 		new = ft_lstnew((*f)(lst->content));
 		if (new == NULL)
 		{
-			ft_lstclear(&first, del);
+			ft_lstclear(&head, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&first, new);
+		ft_lstadd_back(&head, new);
 		lst = lst->next;
 	}
-	return (first);
+	return (head);
 }
