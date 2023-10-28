97061d52f4e61f14455f171209dcc9e5e6e910f8
O.R <orezek@seznam.cz>
2023-10-28 20:46:20 +0200

    Update: complete ft_lstmap function

--- bonus/ft_lstmap.c
+++ bonus/ft_lstmap.c
@@ -3,11 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:39:15 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/18 12:39:17 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/28 19:12:22 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*first;
+
+	first = NULL;
+	while (lst != NULL)
+	{
+		new = ft_lstnew((*f)(lst->content));
+		if (new == NULL)
+		{
+			ft_lstclear(&first, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
+}
