0dfcd873ed5c3f161b81abbfdfbceaae9c179850
O.R <orezek@seznam.cz>
2023-10-29 22:07:46 +0100

    Fix: fix char casting to also include non-standard ASCII characters

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:43 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 16:54:59 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/29 22:03:37 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t size)
 {
-	int	start;
+	int					start;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
+	str1 = (const unsigned char *) s1;
+	str2 = (const unsigned char *) s2;
 	start = 0;
 	while (start < (int) size)
 	{
-		if (*(s1 + start) == '\0' && *(s2 + start) == '\0')
+		if (*(str1 + start) == '\0' && *(str2 + start) == '\0')
 			break ;
-		else if (*(s1 + start) < *(s2 + start))
-			return (*(s1 + start) - *(s2 + start));
-		else if (*(s1 + start) > *(s2 + start))
-			return (*(s1 + start) - *(s2 + start));
+		else if (*(str1 + start) < *(str2 + start))
+			return (*(str1 + start) - *(str2 + start));
+		else if (*(str1 + start) > *(str2 + start))
+			return (*(str1 + start) - *(str2 + start));
 		start++;
 	}
 	return (0);
