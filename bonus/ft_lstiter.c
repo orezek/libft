cf2b98a6866445e9ae42ef8d4a364547f3ecc2f4
O.R <orezek@seznam.cz>
2023-10-29 16:00:22 +0100

    Fix: fix norm compliance

--- bonus/ft_lstiter.c
+++ bonus/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:56 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/28 19:29:34 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/29 15:58:42 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (lst != NULL && f != NULL)
+	{
 		while (lst != NULL)
 		{
 			f(lst->content);
 			lst = lst->next;
 		}
+	}
 }
