fb8e5717ddd3ea08b79c2a2e4e731813e4c125a9
O.R <orezek@seznam.cz>
2023-10-29 14:55:33 +0100

    Test: update test file - add compiler command

--- tests/tst_ft_strtrim.c
+++ tests/tst_ft_strtrim.c
@@ -6,12 +6,12 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 10:42:00 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/10/29 14:48:54 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/29 14:54:41 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-
+//cc tst_ft_strtrim.c ../part2/ft_strtrim.c ../part1/ft_strlen.c ../part1/ft_strlcpy.c ../part1/ft_strncmp.c && ./a.out | cat -e
 int	main(void)
 {
 	char	str[] = " arrakadabra ";
