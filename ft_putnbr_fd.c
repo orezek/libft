ec165f27f678d2484dc7d50257604f7be8e89e63
Oldrich Rezek <orezek@c3r6s3.42prague.com>
2023-10-30 12:44:14 +0100

    Fix: fix memory leak and double free

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:37:40 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/30 11:51:00 by orezek           ###   ########.fr       */
+/*   Updated: 2023/10/30 12:33:53 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	*ptr;
+	char	*or_ptr;
 
 	ptr = ft_itoa(n);
+	or_ptr = ptr;
 	while (*ptr)
 		write(fd, ptr++, 1);
-	free(ptr);
+	free(or_ptr);
 }
