4b69a9828af1588cb122491bf9cb7f9d2bc37ac6
O.R <orezek@seznam.cz>
2023-10-31 22:39:19 +0100

    Fix: fix buffer overflow and edge cases

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,57 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:16 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 16:55:14 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/31 22:29:36 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_start_is_bigger(char *ptr)
+{
+	ptr = malloc(1 * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	*ptr = '\0';
+	return (ptr);
+}
+
+char	*ft_start_is_smaller(char *ptr, unsigned int start,
+				size_t str_size, char const *s)
+{
+	int	i;
+
+	i = 0;
+	ptr = malloc(((str_size - start) + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = str_size - start;
+	s += start;
+	while (i-- > 0)
+		*ptr++ = *s++;
+	*ptr = '\0';
+	return (ptr - (str_size - start));
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char		*ptr;
-	int			size;
+	char	*ptr;
+	size_t	s_len;
+	size_t	str_size;
 
-	size = (int) len;
-	if (start > ft_strlen(s) || len > ft_strlen(s))
-		return (NULL);
-	ptr = malloc(((int)len + 1) * sizeof(char));
+	ptr = NULL;
+	s_len = len;
+	str_size = ft_strlen(s);
+	if (start > str_size)
+		return (ft_start_is_bigger(ptr));
+	if (start <= str_size && s_len > str_size)
+		return (ft_start_is_smaller(ptr, start, str_size, s));
+	ptr = malloc((len + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	s += (start);
 	while (len-- > 0)
 		*ptr++ = *s++;
 	*ptr = '\0';
-	return (ptr - size);
+	return (ptr - s_len);
 }
