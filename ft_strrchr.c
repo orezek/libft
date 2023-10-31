199a24b546bf0ffc87a9f52161a5a0e7334fc265
O.R <orezek@seznam.cz>
2023-10-31 15:34:12 +0100

    Fix: fix pointer arithmetic for cases when the search is for \0

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:58 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 16:55:07 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/10/31 15:22:25 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strrchr(const char *string, int c)
 
 	len = ft_strlen(string);
 	if (c == '\0')
-		return ((char *) string + ft_strlen(string) + 1);
+		return ((char *) string + ft_strlen(string));
 	while (len >= 0)
 	{
 		if (*(string + len) == c)
