17d698591073c44239c6457753788d2b00207c33
O.R <orezek@seznam.cz>
2023-11-01 18:49:51 +0100

    Fix: fix cases for null input and make the code more readable

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:30 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 16:53:56 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/01 18:45:22 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,20 @@ void	*ft_memmove(void *to, const void *from, size_t size)
 {
 	unsigned char		*des;
 	const unsigned char	*src;
-	int					step;
 
 	des = to;
 	src = from;
-	if (des < (src + (size - 1)) && des > src)
+	if (!des & !src)
+		return (to);
+	if (des < src)
 	{
-		step = (int)(size - 1);
-		while (step >= 0)
-		{
-			*(des + step) = *(src + step);
-			step--;
-		}
+		while ((int) size--)
+			*(des++) = *(src++);
 	}
 	else
 	{
-		step = 0;
-		while (step < (int) size)
-		{
-			*(des + step) = *(src + step);
-			step++;
-		}
+		while ((int) size--)
+			*(des + size) = *(src + size);
 	}
-	return ((void *) des);
+	return (to);
 }
