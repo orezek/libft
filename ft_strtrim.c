dba7560fb0536efdf1f53177dd63f494624c69b1
O.R <orezek@seznam.cz>
2023-11-01 08:05:49 +0100

    Fix: fix null or empty check for strings

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:11 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 16:55:11 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/01 08:04:20 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 // "abrakadabra" "abra" -> "kad"
 // "abrakadabra" "a" -> "brakadabr"
-
+// cc -fsanitize=address -Wall -Werror -Wextra ./.tests/tst_ft_strtrim.c
+//ft_strtrim.c ft_strlen.c ft_strlcpy.c ft_strncmp.c && ./a.out | cat -e
 int	is_inside(char const c, char const *set)
 {
 	while (*set)
@@ -25,6 +26,24 @@ int	is_inside(char const c, char const *set)
 	return (0);
 }
 
+int	is_same(char const *s1, char const *set, int s_length)
+{
+	while (is_inside(*s1++, set))
+		s_length--;
+	if (s_length == 0)
+		return (1);
+	return (0);
+}
+
+char	*ft_empty_or_null(char *substr)
+{
+	substr = (char *) malloc(sizeof(char) * (1));
+	if (substr == NULL)
+		return (NULL);
+	*substr = '\0';
+	return (substr);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*substr;
@@ -32,7 +51,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 	int		start;
 
+	substr = NULL;
 	s_length = ft_strlen(s1);
+	if (*s1 == '\0' || is_same(s1, set, s_length))
+		return (ft_empty_or_null(substr));
 	while (is_inside(*(s1 + s_length - 1), set))
 		s_length--;
 	start = 0;
