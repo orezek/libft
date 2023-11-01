fa994052494ed1ac186048f09a37ed13d5c81e27
O.R <orezek@seznam.cz>
2023-11-01 23:46:46 +0100

    Fix: replace int for size_t type

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:58 by orezek            #+#    #+#             */
-/*   Updated: 2023/11/01 22:54:41 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/01 23:33:22 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ size_t	ft_strlcpy(char *to, const char *from, size_t size)
 {
 	size_t		src_size;
 	size_t		sizet;
-	int			check;
 	const char	*end;
 
 	src_size = ft_strlen(from);
-	check = (int) size;
-	if (check < 0)
-		return (src_size);
 	sizet = size;
 	end = from + (sizet - 1);
 	if (sizet > src_size)
