87b00c1ccf99af410ab25873e71a40bd92255739
O.R <orezek@seznam.cz>
2023-11-01 16:15:25 +0100

    Fix: add terminating null character at the end of the new string

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:06 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 18:52:21 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/01 16:10:00 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,11 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	str_len = ft_strlen(s);
 	index = 0;
-	ptr = malloc(str_len + 1 * sizeof(char));
+	ptr = malloc((str_len + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	while (*s)
 		*ptr++ = f(index++, *s++);
+	*ptr = '\0';
 	return (ptr - str_len);
 }
