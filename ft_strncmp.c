cd727fc593c74ef96abd4dc1cb50e821fa5f778b
O.R <orezek@seznam.cz>
2023-11-02 10:40:38 +0100

    Fix: fix casting from int to unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:43 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 22:03:37 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/02 10:31:31 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t size)
 {
-	int					start;
+	unsigned char		start;
 	const unsigned char	*str1;
 	const unsigned char	*str2;
 
 	str1 = (const unsigned char *) s1;
 	str2 = (const unsigned char *) s2;
 	start = 0;
-	while (start < (int) size)
+	while (start < size)
 	{
 		if (*(str1 + start) == '\0' && *(str2 + start) == '\0')
 			break ;
