1f7648096521acea3f6556f593bc5f7a232875aa
O.R <orezek@seznam.cz>
2023-11-02 10:27:31 +0100

    Fix: cast int to unsigned char

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:36:58 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/31 15:22:25 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/02 10:20:39 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 char	*ft_strrchr(const char *string, int c)
 {
-	int	len;
+	int				len;
+	unsigned char	letter;
 
+	letter = (unsigned char) c;
 	len = ft_strlen(string);
-	if (c == '\0')
+	if (letter == '\0')
 		return ((char *) string + ft_strlen(string));
 	while (len >= 0)
 	{
-		if (*(string + len) == c)
+		if (*(string + len) == letter)
 			return ((char *) string + len);
 		len--;
 	}
