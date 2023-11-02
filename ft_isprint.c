4d36c2dd745031a9941626bf98b5e42f40d7b664
O.R <orezek@seznam.cz>
2023-11-02 14:23:24 +0100

    Fix: fix casting and limit to 255 when working with unsigned chars

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:30:06 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/29 16:52:42 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/02 14:21:46 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ static int	ft_isgraph(int c)
 
 	if (c == EOF)
 		return (0);
-	letter = (unsigned char) c;
-	if (letter >= 33 && letter <= 126)
-		return (1);
+	if (c < 256)
+	{
+		letter = (unsigned char) c;
+		if (letter >= 33 && letter <= 126)
+			return (1);
+	}
 	return (0);
 }
 
@@ -30,8 +33,11 @@ int	ft_isprint(int c)
 
 	if (c == EOF)
 		return (0);
-	letter = (unsigned char) c;
-	if (ft_isgraph(letter) || letter == 32)
-		return (1);
+	if (c < 256)
+	{
+		letter = (unsigned char) c;
+		if (ft_isgraph(letter) || letter == 32)
+			return (1);
+	}
 	return (0);
 }
