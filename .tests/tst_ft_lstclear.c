47fe7dd9b4e9cdd33b44316dcba7ae9fe3cf5f88
O.R <orezek@seznam.cz>
2023-11-02 23:34:38 +0100

    Test: update test files

--- .tests/tst_ft_lstclear.c
+++ .tests/tst_ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 18:14:38 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/02 16:28:43 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/02 22:29:38 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,43 @@ void free_str_ptr(void *str)
 }
 int	main(void)
 {
-	char *res = malloc(1000);
-	if (node == NULL)
-		sprintf(res, "(null)");
-	else {
-		sprintf(res, "{node: ptr-> %s}", escape_str((char *)node->content));
-	}
-	return res;
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+	t_list	*head;
 
+	char	*check;
+	char	*str1;
+	char	*str2;
+	char	*str3;
+
+	str1 = ft_strdup("first");
+	str2 = ft_strdup("second");
+	str3 = ft_strdup("third");
+
+	node1 = ft_lstnew(str1);
+	node2 = ft_lstnew(str2);
+	node3 = ft_lstnew(str3);
+
+	head = NULL;
+	ft_lstadd_back(&head, node1);
+	ft_lstadd_back(&head, node2);
+	ft_lstadd_back(&head, node3);
+	// write(1, head->next->next->content, sizeof(head->content));
+	// write(1, "\n", 1);
+	// write(1, node1->content, sizeof(node1->content));
+	// write(1, "\n", 1);
+	//printf("%p", head);
+	check = head->next->content;
+	//printf("%s\n", *check);
+	while(head != NULL)
+	{
+		// write(1, head->content, sizeof(head->content));
+		// write(1, "\n", 1);
+		printf("%p\n", head);
+		head = head->next;
+	}
+	ft_lstclear(&head, free_str_ptr);
+	printf("%p\n", check);
+	printf("%p", head);
 }
