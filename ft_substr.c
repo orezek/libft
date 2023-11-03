2845b5332eecd71c6376015bc1146fd190ac2655
Oldrich Rezek <orezek@c3r5s3.42prague.com>
2023-11-03 13:16:37 +0100

    Fix: fix substr condition when len is == to str size

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:38:16 by orezek            #+#    #+#             */
-/*   Updated: 2023/10/31 22:29:36 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/03 13:05:19 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,26 @@ char	*ft_start_is_bigger(char *ptr)
 char	*ft_start_is_smaller(char *ptr, unsigned int start,
 				size_t str_size, char const *s)
 {
-	int	i;
+	int		i;
+	char	*start_ptr;
 
 	i = 0;
 	ptr = malloc(((str_size - start) + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
+	start_ptr = ptr;
 	i = str_size - start;
 	s += start;
 	while (i-- > 0)
 		*ptr++ = *s++;
 	*ptr = '\0';
-	return (ptr - (str_size - start));
+	return (start_ptr);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
+	char	*start_ptr;
 	size_t	s_len;
 	size_t	str_size;
 
@@ -49,14 +52,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str_size = ft_strlen(s);
 	if (start > str_size)
 		return (ft_start_is_bigger(ptr));
-	if (start <= str_size && s_len > str_size)
+	if (start <= str_size && s_len >= str_size)
 		return (ft_start_is_smaller(ptr, start, str_size, s));
 	ptr = malloc((len + 1) * sizeof(char));
+	start_ptr = ptr;
 	if (ptr == NULL)
 		return (NULL);
 	s += (start);
 	while (len-- > 0)
 		*ptr++ = *s++;
 	*ptr = '\0';
-	return (ptr - s_len);
+	return (start_ptr);
 }
